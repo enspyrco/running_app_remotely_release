b32b96e351c853983c2a2514f00ccf8f426de0f0
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-18 23:48:23 +1100

    Delete build artifacts & add "flutter build web"
    - added a "flutter build web" step before "flutter run"
    - deleted .dart_tool & app/build/web directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "Downloading dependencies & running the app."
-cd /app && dart pub get && flutter run -d web-server --release --web-hostname 0.0.0.0 --web-port 8080
+cd /app && flutter pub get && flutter build web && flutter run --verbose -d web-server --release --web-hostname 0.0.0.0 --web-port 8080
